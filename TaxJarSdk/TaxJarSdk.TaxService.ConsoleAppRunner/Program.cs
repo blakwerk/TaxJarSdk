0b8f7fcc9104babb1c23f4bc17cc679941b0dcb3
Denton <matthew.denton@billgo.com>
2021-06-19 08:00:06 -0600

    Remove old console runner Add new console runner

---
+++ TaxJarSdk/TaxJarSdk.TaxService.ConsoleAppRunner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TaxJarSdk.TaxService.ConsoleAppRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
